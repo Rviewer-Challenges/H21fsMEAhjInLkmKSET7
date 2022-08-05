f9a77c7a33eab86ffbfb731a43c47b8d1a8002ed
Uciel89 <ucielprogramador21@gmail.com>
2022-08-05 14:56:05 -0300

    Programmer Chat

---
+++ ProgrammerChat/app/src/main/java/com/ucielcorp/programmerchat/views/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.ucielcorp.programmerchat.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ucielcorp.programmerchat.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
